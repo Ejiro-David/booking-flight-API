54f35d83c2ce66c0fa41b40c097891a5d1b7956f
EJ-David <daviderhabor.de@gmail.com>
2022-11-03 19:56:39 +0100

    nothing much

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { json } = require("express");
 // const flights = require("./controllers/flightController");
 const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
-let id;
+// let id;
 let flight_data = [
   {
     id: 1,
